6d55c18e502937c156eec95e13b5495e4cba25fd
Nurul Andini <39187373+nurulandini@users.noreply.github.com>
2018-11-25 15:48:56 +0700

    create loginsiswa.php
    file loginsiswa.php di folder userview

---
+++ application/views/userview/loginsiswa.php
@@ -0,0 +1,26 @@
+<div class="row">
+	<div class="col-md-3">
+	<table>
+		<tr>
+			<th>Login Siswa</th>
+		</tr>
+		<tr>
+			<td>
+				<label>
+				Username	
+				<input type="text" name="username" placeholder="Username">
+				</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>
+					Password
+					<input type="password" name="password" placeholder="Password">
+				</label>
+			</td>
+		</tr>
+	</table>
+	
+	</div>
+</div>
