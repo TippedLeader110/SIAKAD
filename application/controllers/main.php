b719b0c4bead4524f4cb99c875c9de1411885e43
TippedLeader110 <bayhaqi101@gmail.com>
2018-11-25 15:27:48 +0700

    Update Halaman utama

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,6 +5,7 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('layout/home');
+		$data['page']='userview/home';
+		$this->load->view('layout/main', $data);
 	}
 }
