1ae4b98328fd0175e295490877d63f756f771ee9
Adelia Salmah Siregar <adeliasalmahsiregar@gmail.com>
2018-12-01 14:18:25 +0700

    create daftarnilai.php
    didalam folder views\userview

---
+++ application/views/userview/daftarnilai.php
@@ -0,0 +1,47 @@
+<div class="row">
+			<div class="col-md-12">
+				<div class="container">
+					<h3 style="color:#4da6ff;margin-left:30px;">Daftar Nilai</h3>
+
+					<div class="col-md-3">
+						<div class="row">
+							<div class="container">
+								<h3>DATA NAMA</h3>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="row">
+							<div class="container">
+								<h3>DATA NIS</h3>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="col-md-4">
+								<span></span>
+								<a class="btn btn-primary btn-outline" href="#">Nilai UAS <i class="icon-arrow-right22"></i></a>
+					</div>
+
+					<div class="col-md-4">
+								<span></span>
+								<a class="btn btn-primary btn-outline" href="#">Nilai UTS <i class="icon-arrow-right22"></i></a>
+					</div>
+
+					<div class="col-md-4">
+								<span></span>
+								<a class="btn btn-primary btn-outline" href="#">Tugas<i class="icon-arrow-right22"></i></a>
+					</div>
+
+					<div class="col-md-4">
+								<span></span>
+								<a class="btn btn-primary btn-outline" href="#">Rata-rata<i class="icon-arrow-right22"></i></a>
+					</div>
+
+
+					
+				</div>
+			</div>
+</div>
