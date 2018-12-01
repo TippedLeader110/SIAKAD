7e67691d8448336c9fb7ace4b9f80f483878d888
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-01 13:16:56 +0700

    Update mainmodel.php

--- application/models/mainmodel.php
+++ application/models/mainmodel.php
@@ -7,4 +7,42 @@ class Mainmodel extends CI_Model {
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function lsiswa($user, $pass)
+	{
+		$this->db->where(['user' => $user, 'pass' => $pass]);
+		$query = $this->db->get('siswa');
+		$query = $query->num_rows();
+		if ($query==0) {
+			redirect(''); //redirect gagal
+		}
+		else if ($query==1) {
+		$this->db->where(['user' => $user, 'pass' => $pass]);
+		$query = $this->db->get('siswa');
+		$data = $query->result();
+		foreach ($data as $key => $w) {
+			}
+			$this->session->user = $w->nama;
+			redirect(''); //redirect berhasil
+	}
+
+	public function lguru($user, $pass)
+	{
+		$this->db->where(['user' => $user, 'pass' => $pass]);
+		$query = $this->db->get('guru');
+		$query = $query->num_rows();
+		if ($query==0) {
+			redirect(''); //redirect gagal
+		}
+		else if ($query==1) {
+		$this->db->where(['user' => $user, 'pass' => $pass]);
+		$query = $this->db->get('guru');
+		$data = $query->result();
+		foreach ($data as $key => $w) {
+			}
+			$this->session->user = $w->nama;
+			redirect(''); //redirect berhasil
+	}
+
+
 }
