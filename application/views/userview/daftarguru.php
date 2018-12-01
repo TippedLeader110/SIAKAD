900446950985390f0fc0eef6979d5f4c5bb4b76a
melasalsabil <39105281+melasalsabil@users.noreply.github.com>
2018-12-01 14:09:04 +0700

    create daftarguru.php
    pada folder view/userview/

---
+++ application/views/userview/daftarguru.php
@@ -0,0 +1,26 @@
+<div class="container">
+  <div class="row">
+    Daftar Guru
+  </div>
+<div class="row">
+  <div class="col-lg-6">
+    <div class="input-group">
+      <table>
+        <tr>
+          <td>
+            Ketikkan Nama Guru Yang Ingin Anda Cari
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" class="form-control" placeholder="Ketikkan disini">
+            <button type="button" class="btn btn-default btn-lg">
+              <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+            </button>
+          </td>
+        </tr>
+      </table>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+</div><!-- /.row -->
+</div>
