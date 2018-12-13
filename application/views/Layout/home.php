ff3891049500468bc4da9bb75a93852c217edfaf
TippedLeader110 <bayhaqi101@gmail.com>
2018-12-13 15:25:56 +0700

    navbarkiri edit

--- application/views/Layout/home.php
+++ application/views/Layout/home.php
@@ -17,7 +17,7 @@
 	</div>
 	<div class="container-fluid">
 	<div class="row" style="margin-top: 20px">
-		<?php include 'navbarkiri.php'; ?>
+		<?php include 'navbar-kiri.php'; ?>
 		<!-- <div class="col-md-3">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
