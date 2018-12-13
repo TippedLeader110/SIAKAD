2102f1b42bcba78e92272cc683f5d54b6d13c994
usokun <alihidayat278@gmail.com>
2018-12-13 17:18:54 +0900

    Create navbar-kiri.php
    navbar-kiri(Siswa)

---
+++ application/views/Layout/navbar-kiri.php
@@ -0,0 +1,36 @@
+<!-- ini navbar-kiri-siswa-->
+
+<div class="col-md-3" style="background-color: #8DB3DA; margin-left: 20px; margin-top: 0px;">
+      <div class="profile" align="center">
+        <div class="circle">
+          <img src="icon-profile.png" style="max-width: 100px; max-height: 100px;">
+        </div>
+        <h3 align="center" style="margin-top: 20px; color: white;"><b>Selamat Datang,</b></h3>
+        <div class="field-wel">
+          <h4 align="center">
+            <b>nama_user</b>
+          </h4>
+        </div>
+        <div class="field-menu">
+          <div class="row">
+            <div style="max-width: 200px; min-width: 170px; background-color: #ebebe0; min-height: 35px;">
+              <h4>a</h4>
+            </div>
+            <div style="max-width: 200px; min-width: 170px; background-color: #ebebe0; min-height: 35px;">
+              <h4>a</h4>
+            </div>
+            <div style="max-width: 200px; min-width: 170px; background-color: #ebebe0; min-height: 35px;">
+              <h4>a</h4>
+            </div>
+            <div style="max-width: 200px; min-width: 170px; background-color: #ebebe0; min-height: 35px;">
+              <h4>a</h4>
+            </div>
+            <div style="max-width: 200px; min-width: 170px; background-color: #ebebe0; min-height: 35px;">
+              <h4>a</h4>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      
+    </div>
