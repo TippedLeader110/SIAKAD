e850851eef563e53d75e9c5396e46ebc95cecde3
Ihsan <m.ihsanmaulana562@gmail,.com>
2018-12-15 18:54:15 +0700

    update tampilan login guru dan siswa
    by: nurul
    Co-Authored-By: Nurul Andini <nurulandini@users.noreply.github.com>

--- application/views/userview/loginguru.php
+++ application/views/userview/loginguru.php
@@ -1,58 +1,59 @@
-<div class="row" style="margin-bottom: -100px;">
-		<div class="col-md-4 offset-md-4">
-		<table border="2">
+<div class="row" style="margin-top: 30px;margin-bottom: -50px;z-index: -1">
+		<div class="col-md-4 offset-md-4" style="background: white;padding-top: 2em;padding-bottom: 2em">
+		<table  width="300" align="center">
+			<tr>
+				<td align="center"><div class="circle">
+					<img src="<?php echo base_url()?>img/icon/icon-profil-guru.png" style="max-width: 100px; max-height: 100px;">
+				</div></td>
+			</tr>
 			<tr>
 				<td>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</td>
-			</tr>
-		</table>
-		
-	</div>
+					<div>
+				<form>
+					<table align="center">
+						<tr align="center">
+							<th colspan="2">Login Siswa</th>
+						</tr>
+						<tr><td></td></tr>
+						<tr>
+					<td>
+						<label>Username</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="text" name="username" placeholder="Username">
+					</td>
+					<td></td>
+					<td><span style="background:white;border-radius: 50%;">?</span></td>
+				</tr>		
+				<tr>
+					<td>
+						<label>Password</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="password" name="password" placeholder="Password" >
+					</td>
+					<td></td>
+					<td><span style="background:white;border-radius: 50%;">?</span></td>
+				</tr>
+			</table>
+			<div style="padding-top: 2em;">
+				<center>
+				<button class="btn btn-success">Masuk</button>
+				</center>
+				</form>
+					</div>
+						</td>
+					</tr>
+				</table>
+				
+			</div>
+	
 </div>
 <div class="row">
-	<div class="col-md-4 offset-md-4" style="background: black; margin-top: 2em; margin-bottom: 2em; padding-bottom: 4em; padding-top: 2em;">
-		<form>
-			<table align="center">
-				<tr align="center">
-					<th colspan="2">Login Siswa</th>
-				</tr>
-				<tr><td></td></tr>
-				<tr>
-			<td>
-				<label>Username</label>
-			</td>
-		</tr>
-		<tr>
-			<td>
-			<input type="text" name="username" placeholder="Username">
-			</td>
-			<td></td>
-			<td><span style="background:white;border-radius: 50%;">?</span></td>
-		</tr>		
-		<tr>
-			<td>
-				<label>Password</label>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<input type="password" name="password" placeholder="Password" >
-			</td>
-			<td></td>
-			<td><span style="background:white;border-radius: 50%;">?</span></td>
-		</tr>
-	</table>
-	<div style="padding-top: 2em;">
-		<center>
-		<button class="btn btn-success">Submit</button>
-		</center>
-		</form>
-			</div>
+	
 	</div>
 </div>
