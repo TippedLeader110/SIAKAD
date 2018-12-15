a8c613d1b3cce4062550d55156bf5565e7a33646
Nurul Andini <39187373+nurulandini@users.noreply.github.com>
2018-12-15 20:19:40 +0700

    aku export database
    di dalam folder SIAKAD nama file siakad.sql

--- application/views/userview/home.php
+++ application/views/userview/home.php
@@ -23,15 +23,45 @@
 						<h3>Login</h3>
 					</div>
 				</div>
+					
 				<div class="row">
-					<div class="container">
-						<div class="col-md-6">
-							GUURUU
+				<div class="container">
+					<div class="row" style="background: white;padding-bottom: 2em;padding-top: 2em;">
+						<div style="padding-left: 15px">
+							<table border="1">
+								<tr>
+									<td style="background: grey;  padding: 5px 5px 5px 5px;background: transparent;">
+										<div class="circle">
+											<img src="<?php echo base_url()?>img/icon/student-icon.png" style="max-width: 100px; max-height: 100px;">
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td align="center" style="background: grey; margin-top: 5px;background: transparent;opacity: 100%">
+									<img src="<?php echo base_url()?>img/icon/login-icon.png" style="max-width: 30px; max-height: 30px;">
+									</td>
+								</tr>
+							</table>
 						</div>
-						<div class="col-md-6">
-							SIISSWAA
+						<div style="margin-left: 20px;">
+							<table border="1">
+								<tr>
+									<td style="background: grey;  padding: 5px 5px 5px 5px;">
+										<div class="circle">
+											<img src="<?php echo base_url()?>img/icon/student-icon.png" style="max-width: 100px; max-height: 100px;background: transparent;opacity: 100%">
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td align="center" style="background: grey; margin-top: 5px;">
+									<img src="<?php echo base_url()?>img/icon/login-icon.png" style="max-width: 30px; max-height: 30px;">
+									</td>
+								</tr>
+							</table>
 						</div>
+
 					</div>
 				</div>
 			</div>
+		</div>
 		</div>
