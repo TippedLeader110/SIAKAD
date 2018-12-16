b11e8ee1936b11d1a38f570b37e71c4d491d0180
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-16 18:08:04 +0700

    inputnilaiwalikelas

---
+++ application/views/userview/inputnilai.php
@@ -0,0 +1,167 @@
+<div class="container" style="border-width:0px; background-color:#DCDDE1; padding-top: 10px; padding-bottom: 10px;">
+<div class="container" style="border-width:1px; background-color:#DCDDE1;">
+	<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;">
+		<h5 align="center"><b>DATA SISWA</b></h5>	
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-6">
+					<table style="min-width: 500px;">
+						<tr>
+							<td>Nama Siswa</td>
+							<td><input ></td>
+						</tr>
+						<tr>
+							<td>NIS</td>
+							<td><input ></td>
+						</tr>
+						<tr>
+							<td>Jurusan</td>
+							<td>
+								<select class="form-control" style="width: 200px;">
+								<option value="jurusan">-Pilih Jurusan-</option>
+								<option value="jurusan">Ilmu Pengetahuan Alam</option>
+								<option value="jurusan">Ilmu Pengetahuan Sosial</option>
+								</select> 
+							</td>
+						</tr>
+						<tr>
+							<td>Kelas</td>
+							<td>
+								<select class="form-control" style="width: 200px;">
+								<option value="kelas">-Pilih Kelas-</option>
+								<option value="kelas">X IPA 1</option>
+								<option value="kelas">X IPA 2</option>
+								<option value="kelas">X IPA 3</option>
+								<option value="kelas">X IPS 1</option>
+								<option value="kelas">X IPS 2</option>
+								<option value="kelas">X IPS 3</option>
+								<option value="kelas">XI IPA 1</option>
+								<option value="kelas">XI IPA 2</option>
+								<option value="kelas">XI IPA 3</option>
+								<option value="kelas">XI IPS 1</option>
+								<option value="kelas">XI IPS 2</option>
+								<option value="kelas">XI IPS 3</option>
+								<option value="kelas">XII IPA 1</option>
+								<option value="kelas">XII IPA 2</option>
+								<option value="kelas">XII IPA 3</option>
+								<option value="kelas">XII IPS 1</option>
+								<option value="kelas">XII IPS 2</option>
+								<option value="kelas">XII IPS 3</option>
+								</select>
+							</td>
+						</tr>
+					</table>
+				</div>
+				<div class="col-md-6">
+					<table style="min-width: 500px;">
+						<tr>
+							<td>Mata Pelajaran</td>
+							<td>
+								<select class="form-control" style="width: 200px;">
+									<option value="matpel">-Pilih Mata Pelajaran-</option>
+									<option value="matpel">Akidah Akhlak</option>
+									<option value="matpel">Al-Qur'an Hadist</option>
+									<option value="matpel">Bahasa Arab</option>
+									<option value="matpel">Bahasa Indonesia</option>
+									<option value="matpel">Bahasa Inggris</option>
+									<option value="matpel">Bahasa Jerman Peminatan</option>
+									<option value="matpel">Biologi Peminatan</option>
+									<option value="matpel">Ekonomi Peminatan</option>
+									<option value="matpel">Fikih</option>
+									<option value="matpel">Fisika Peminatan</option>
+									<option value="matpel">Geografi</option>
+									<option value="matpel">Kimia Peminatan</option>
+									<option value="matpel">Matematikan</option>
+									<option value="matpel">Pendidikan Jasmani</option>
+									<option value="matpel">Pendidikan Kewarganegaraan</option>
+									<option value="matpel">Prakarya Kewirausahaan</option>
+									<option value="matpel">Sejarah</option>
+									<option value="matpel">Sejarah Indonesia</option>
+									<option value="matpel">Sejarah Kebudayaan Islam</option>
+									<option value="matpel">Seni Budaya</option>
+									<option value="matpel">Sosiologi</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>Semester</td>
+							<td>
+								<select class="form-control" style="width: 200px;">
+									<option value="semester">-Pilih Semester-</option>
+									<option value="semester">Semester Ganjil</option>
+									<option value="semester">Semester Genap</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>Tahun Pelajaran</td>
+							<td><input ></td>
+						</tr>
+					</table> 
+				</div>
+			</div>
+		</div>	
+		<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;"></div>
+	</div>
+</div>
+</div>
+
+<div style="width:200px; height:30px; margin-left:350px; margin-top:-15px;"></div>
+<div class="container" style="border-width:0px; background-color:#DCDDE1; padding-top: 10px; padding-bottom: 10px;">
+<div class="container" style="border-width:1px; background-color:#DCDDE1;">
+	<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;">
+		<h5 align="center"><b>DATA NILAI</b></h5>	
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-6">
+					<table style="min-width: 500px;">
+						<tr>
+							<td>Tugas 1</td>
+							<td><input ></td>
+						</tr>
+						<tr>
+							<td>Tugas 2</td>
+							<td><input ></td>
+						</tr>
+						<tr>
+							<td>Tugas 3</td>
+							<td><input ></td>
+						</tr>
+					</table>
+				</div>
+	 
+				<div class="col-md-6">
+					<table style="min-width: 500px;">
+						<tr>
+							<td>Nilai UTS</td>
+							<td><input ></td>
+						</tr>
+						<tr>
+							<td>Nilai UAS</td>
+							<td><input ></td>
+						</tr>
+					</table> 
+				</div>
+			</div>
+		</div>	
+		<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;"></div>
+	</div>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-md-2">
+		<div class="container" style="border-width:0px; color:white; font-weight:500; margin-left: 365px; margin-top: 30px;">
+			<center><b><a href="#" class="btn btn-primary">Simpan</a></b></center>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="container" style="border-width:0px; font-weight:500; color:white; margin-left: 290px; margin-top: 30px;">
+			<center><b><a href="#" class="btn btn-danger">Batal</a></b></center>
+		</div>
+	</div>
+</div>
