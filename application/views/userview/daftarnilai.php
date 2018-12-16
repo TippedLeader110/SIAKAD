fe81584f5377ae3a1224543fcd3c5961c45d4bd6
Adelia Salmah Siregar <adeliasalmahsiregar@gmail.com>
2018-12-16 18:24:38 +0700

    Update daftarnilai.php

--- application/views/userview/daftarnilai.php
+++ application/views/userview/daftarnilai.php
@@ -7,34 +7,23 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="row">
-			<div style="background-color: white; border-color: black; max-width: 200px; margin-right: 50px; margin-left: 350px; margin-top: 10px; margin-bottom: 10px; border-width:2px; border-style: solid;">
-				<h4 align="center" style="padding: 5px 5px 5px 5px;">DATA NIS</h4>
+			<div style="background-color: white; border-color: black; max-width:300px; margin-right: 50px; margin-left: 200px; margin-top: 80px; margin-bottom: 10px; border-width:2px; border-style: solid;width: 550px;height:40px;">
+				<h4 align="center" ">DATA NAMA</h4>
 			</div>
-			<div style="background-color: white; border-color: black; max-width: 200px; margin-left: 20px;  margin-top: 10px;  margin-bottom: 10px; border-width:2px; border-style: solid;">
-				<h4 align="center" style="padding: 5px 5px 5px 5px;">DATA NAMA</h4>
+			<div style="background-color: white; border-color: black; max-width: 300px; margin-left: 20px;  margin-top: 80px;  margin-bottom: 10px; border-width:2px; border-style: solid;width: 550px;height: 40px">
+				<h4 align="center">DATA NIS</h4>
 			</div>
 		</div>
 
-		<div style="display:inline-block;text-align: center;margin-left: 30;margin-top: 50px;margin-right:20  ">
-			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black;"></div>
-		</div>
-		<!-- <div style="text-align: center;display:table-row;margin-left: 20px; margin-right: 50px;">
-			<div style="border: 1px solid #CCC;display: table-cell; width: 150px;height: 40px;border-color: black;">Niai UAS</div>
-		</div>
-  -->
-		<div style="text-align: center;display: inline-block;margin-left: 50px; margin-right: 0px;margin-top: 50px ">
-			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black;"></div>
-		</div>
-		<!-- <div class="table-row strip" style="display:table-row;text-align: center;margin-left: 10px">
-			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 150px;height: 40px;border-color: black;">Niai UAS</div>
-		</div> -->
-
-		<div style="text-align: center;display: inline-block;margin-left: 50px; margin-right: 20px;margin-top: 50px ">
-			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black; "></div>
-		</div>
-
-		<div style="text-align: center;display: inline-block;margin-left: 20px; margin-right: 10px;margin-top: 50px ">
-			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black; "></div>
+		<div style="display: inline-block;width: 200px;height: 200px;margin-top: 50px;margin-right:35px;margin-left:19px;border: 3px solid #8DB3DA;"></div>
+		<div style="display: inline-block;width: 200px;height: 200px;margin-top: 50px;margin-right:35px;border: 3px solid #8DB3DA;"></div>
+		<div style="display: inline-block;width: 200px;height: 200px;margin-top: 50px;margin-right:35px;border: 3px solid #8DB3DA;"></div>
+		<div style="display: inline-block;width: 200px;height: 200px;margin-top: 50px;margin-right:35px;border: 3px solid #8DB3DA;"></div>
+		<div style="display: inline-block;width: 200px;height: 40px;margin-top: 10px;margin-right:35px;margin-left:19px;border: 3px solid #8DB3DA;"><center><h4>Nilai UAS</h4></center></div>
+		<div style="display: inline-block;width: 200px;height: 40px;margin-top: 10px;margin-right:35px;border: 3px solid #8DB3DA;"><center><h4>Nilai UTS</h4></center></div>
+		<div style="display: inline-block;width: 200px;height: 40px;margin-top: 10px;margin-right:35px;border: 3px solid #8DB3DA;"><center><h4>Tugas</h4></center></div>
+		<div style="display: inline-block;width: 200px;height: 40px;margin-top: 10px;margin-right:35px;border: 3px solid #8DB3DA;"><center><h4>Rata-Rata</h4></center></div>
+			
 		</div>
 	</div>
 </div>
