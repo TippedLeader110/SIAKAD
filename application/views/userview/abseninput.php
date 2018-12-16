7718c082ffe03302236d122a35bc6074daf17092
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-16 23:42:40 +0700

    Update abseninput.php

--- application/views/userview/abseninput.php
+++ application/views/userview/abseninput.php
@@ -79,6 +79,8 @@
 </div>
 </div>
 
+<div style="width:200px; height:30px; margin-left:350px; margin-top:-15px;"></div>
+
 <div class="container" style="border-width:0px; background-color:#DCDDE1; padding-top: 10px; padding-bottom: 10px;">
 <div class="container" style="border-width:1px; background-color:#DCDDE1;">
 	<div style="background-color:#DCDDE1; width:250px; height:30px; margin-left:325px; margin-top:-15px;">
