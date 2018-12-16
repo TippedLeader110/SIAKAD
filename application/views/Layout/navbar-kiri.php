d8dd8d07058dadea9c08ed1de7444d917ef67825
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-16 14:50:14 +0700

    saldik

--- application/views/Layout/navbar-kiri.php
+++ application/views/Layout/navbar-kiri.php
@@ -46,7 +46,7 @@
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
-                <h5 style="color: black; text-align: left;" class=<?php echo $guru?>>Daftar echo</h5>
+                <h5 style="color: black; text-align: left;" class=<?php echo $guru?>>Daftar Guru</h5>
                </div>
               </a>
               <a href="#">
