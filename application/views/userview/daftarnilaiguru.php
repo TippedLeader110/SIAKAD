e709b6a8cb7e22bff9901b0f5ded88daac62ab35
Adelia Salmah Siregar <adeliasalmahsiregar@gmail.com>
2018-12-16 16:41:05 +0700

    Create daftarnilaiguru.php
    views\userview\daftarnilaiguru
    
    daftarnilai.php rename menjadi daftarnilaiguru.php

---
+++ application/views/userview/daftarnilaiguru.php
@@ -0,0 +1,40 @@
+<div class="row">
+	<div class="col-md-12">
+		
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="row">
+			<div style="background-color: white; border-color: black; max-width: 200px; margin-right: 50px; margin-left: 350px; margin-top: 10px; margin-bottom: 10px; border-width:2px; border-style: solid;">
+				<h4 align="center" style="padding: 5px 5px 5px 5px;">DATA NIS</h4>
+			</div>
+			<div style="background-color: white; border-color: black; max-width: 200px; margin-left: 20px;  margin-top: 10px;  margin-bottom: 10px; border-width:2px; border-style: solid;">
+				<h4 align="center" style="padding: 5px 5px 5px 5px;">DATA NAMA</h4>
+			</div>
+		</div>
+
+		<div style="display:inline-block;text-align: center;margin-left: 30;margin-top: 50px;margin-right:20  ">
+			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black;"></div>
+		</div>
+		<!-- <div style="text-align: center;display:table-row;margin-left: 20px; margin-right: 50px;">
+			<div style="border: 1px solid #CCC;display: table-cell; width: 150px;height: 40px;border-color: black;">Niai UAS</div>
+		</div>
+  -->
+		<div style="text-align: center;display: inline-block;margin-left: 50px; margin-right: 0px;margin-top: 50px ">
+			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black;"></div>
+		</div>
+		<!-- <div class="table-row strip" style="display:table-row;text-align: center;margin-left: 10px">
+			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 150px;height: 40px;border-color: black;">Niai UAS</div>
+		</div> -->
+
+		<div style="text-align: center;display: inline-block;margin-left: 50px; margin-right: 20px;margin-top: 50px ">
+			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black; "></div>
+		</div>
+
+		<div style="text-align: center;display: inline-block;margin-left: 20px; margin-right: 10px;margin-top: 50px ">
+			<div class="col" style="border: 1px solid #CCC;display: table-cell; width: 200px;height: 200px;border-color: black; "></div>
+		</div>
+	</div>
+</div>
