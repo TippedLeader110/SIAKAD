73c14274342045bd16a460120e873a55533e0083
Nurul Andini <39187373+nurulandini@users.noreply.github.com>
2018-12-16 15:37:16 +0700

    update tampilan home.php

--- application/views/userview/home.php
+++ application/views/userview/home.php
@@ -1,15 +1,15 @@
 	<div class="row">
-			<div class="col-md-12">
-			<div class="container">
-				<h4>Selamat Datang di</h4>
-				<h3>Sistem Informasi Akademik</h3>
+			<div class="col-md-12" >
+			<div class="container" style="background: white;font-color:blue;border-width: 1px;border-color: #d6d6c2;padding-top: 1em;padding-bottom: 1em;">
+				<h4 style="color: #2F669F;">Selamat Datang di</h4>
+				<h3 style="color: #2F669F">Sistem Informasi Akademik</h3>
 			</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-8">
-				<div class="container">
-					<h3>Tentang</h3>
+				<div class="container" style="background: white;border-width: 1px;border-color: #d6d6c2;padding-bottom: 123px">
+					<h3  style="color: #2F669F;">Tentang</h3>
 					<p>Sistem Informasi Akademin (SIAKAD) adalah sebuah aplikasi yang dibuat untuk Siswa/i yang dapat diakases oleh orangtua siswa/i dan guru/wali kelas agar dapat mengakses informasi seputar lingkungan akademik secara online dimana dan kapan saja.</p>
 					<p>
 						Siswa dapat mengakses informasi data diri yang sudah terdaftar di sekolah dan juga informasi mengenai pengembangan prestasi akademkinya di sekolah dan orang tua siswa dapat memantau perkembangan anaknya di sekolah
@@ -17,49 +17,94 @@
 					<button class="btn btn-default btn-sm"><span class="glyphicon glyphicon-menu-right"></span>Pelajar lebih lanjut</button>
 				</div>
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<div class="row">
-					<div class="container">
-						<h3>Login</h3>
+					<div class="container" style="background: #2F669F;border-width: 1px;border-color: #d6d6c2;padding-bottom: 1em; margin-right: 16px;">
+						<div class="row" style="margin-left: 5px;padding-top: 5px">
+							<img src="<?php echo base_url()?>img/icon/login-icon.png" style="max-width: 100px; max-height: 100px;height: 30px;width: 30px">
+							<h4 style="color: white">&nbsp;Login</h4>
+						</div>
 					</div>
 				</div>
 					
 				<div class="row">
-				<div class="container">
-					<div class="row" style="background: white;padding-bottom: 2em;padding-top: 2em;">
-						<div style="padding-left: 15px">
-							<table border="1">
+				<div class="container" style="border-width: 1px;border-color:#d6d6c2;  margin-right: 16px;">
+					<div class="row" style="background: white;padding-bottom: 3em;padding-top: 2em;border-color: #d6d6c2;margin-top: -20px">
+						<div style="padding-left: 50px">
+							<table style="border-color: #d6d6c2">
 								<tr>
-									<td style="background: grey;  padding: 5px 5px 5px 5px;background: transparent;">
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;border-color: #d6d6c2" width="120px" height="120px">
 										<div class="circle">
-											<img src="<?php echo base_url()?>img/icon/student-icon.png" style="max-width: 100px; max-height: 100px;">
+											<center>
+												<img src="<?php echo base_url()?>img/icon/icon-profil-guru.png" style="max-width: 100px; max-height: 100px;">
+											</center>
 										</div>
-									</td>
-								</tr>
-								<tr>
-									<td align="center" style="background: grey; margin-top: 5px;background: transparent;opacity: 100%">
-									<img src="<?php echo base_url()?>img/icon/login-icon.png" style="max-width: 30px; max-height: 30px;">
 									</td>
 								</tr>
 							</table>
 						</div>
 						<div style="margin-left: 20px;">
-							<table border="1">
+							<table style="border-color: #d6d6c2">
 								<tr>
-									<td style="background: grey;  padding: 5px 5px 5px 5px;">
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;" width="120px" height="120px">
 										<div class="circle">
-											<img src="<?php echo base_url()?>img/icon/student-icon.png" style="max-width: 100px; max-height: 100px;background: transparent;opacity: 100%">
+											<img src="<?php echo base_url()?>img/icon/icon-profil-guru.png" style="max-width: 100px; max-height: 100px;">
 										</div>
-									</td>
-								</tr>
-								<tr>
-									<td align="center" style="background: grey; margin-top: 5px;">
-									<img src="<?php echo base_url()?>img/icon/login-icon.png" style="max-width: 30px; max-height: 30px;">
 									</td>
 								</tr>
 							</table>
 						</div>
-
+					</div>
+					<div class="row" style="background: white;padding-bottom: 2em;padding-top: 2em;border-color: #d6d6c2; margin-top: -75px;">	
+						<div style="padding-left: 50px">
+							<table style="border-color: #d6d6c2">
+								<tr>
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;border-color: #d6d6c2" width="120px" align="center">
+									<h6 style="font-family:'Calibri',Times,sans-sherif"><b>Guru/Wali Kelas</b></h6>
+									</td>
+								</tr>
+							</table>
+						</div>
+						<div style="padding-left: 20px">
+							<table style="border-color: #d6d6c2">
+								<tr>
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;border-color: #d6d6c2" width="120px" height="30px" align="center">
+									<h6 style="font-family:'Calibri',Times,sans-sherif"><b>Siswa/i</b></h6>
+									</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+					<div class="row" style="background: white;padding-bottom: 2em;padding-top: 2em;border-color: #d6d6c2; margin-top: -50px;">
+						<div style="padding-left: 50px">
+							<table style="border-color: #d6d6c2">
+								<tr>
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;" width="120px" height="30px" align="center">
+										<img src="<?php echo base_url()?>img/icon/login-icon.png" style="height: 20px;width: 20px">
+									</td>
+								</tr>
+							</table>
+						</div>
+						<div style="margin-left: 20px;">
+							<table style="border-color: #d6d6c2">
+								<tr>
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;" width="120px" height="30px" align="center">
+											<img src="<?php echo base_url()?>img/icon/login-icon.png" style="height: 20px;width: 20px">
+									</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+					<div class="row" style="background: white;padding-bottom: 2em;padding-top: 2em;border-color: #d6d6c2; margin-top: -50px;">
+						<div style="padding-left: 50px">
+							<table style="border-color: #d6d6c2">
+								<tr>
+									<td style="background: rgba(128,128,128,0.5);  padding: 5px 5px 5px 5px;" width="260px" height="30px" align="center">
+										<a href="#">Belum punya akun ?</a>
+									</td>
+								</tr>
+							</table>
+						</div>						
 					</div>
 				</div>
 			</div>
