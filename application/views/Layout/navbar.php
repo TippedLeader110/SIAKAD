a2cff5a1a7a07c6d61e8a7739fbe4b03de160035
melasalsabil <39105281+melasalsabil@users.noreply.github.com>
2018-12-16 12:53:35 +0700

    navbar change

--- application/views/Layout/navbar.php
+++ application/views/Layout/navbar.php
@@ -31,8 +31,8 @@
       </li> -->
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="Cari" placeholder="Cari" aria-label="Cari">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
     </form>
   </div>
 </nav>
