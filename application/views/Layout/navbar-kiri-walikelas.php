495b8b1e6a19d74915223be11149e5af0672d70a
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-17 00:29:30 +0700

    login beta, navbar kiri guru, heading nilai uas/uts/tugas

--- application/views/Layout/navbar-kiri-walikelas.php
+++ application/views/Layout/navbar-kiri-walikelas.php
@@ -13,14 +13,13 @@
 
         <div class="field-menu scrollable">
           <div class="row">
-              <a href="http://localhost/SIAKAD/home/siswa">
+              <a href="http://localhost/SIAKAD/home/guru">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
                 <h5 style="color: black; text-align: left;" class=<?php echo $beranda?>>Beranda</h5>
                </div>
               </a>
-             
               <a href="http://localhost/SIAKAD/Nilai/siswa">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
@@ -28,35 +27,35 @@
                 <h5 style="color: black; text-align: left;" class=<?php echo $nilai?>>Daftar Nilai</h5>
                </div>
               </a>
-              <a href="http://localhost/SIAKAD/Nilai/raport">
+              <a href="http://localhost/SIAKAD/Nilai/walikelas">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
                 <h5 style="color: black; text-align: left;" class=<?php echo $rapot?>>Input Nilai</h5>
                </div>
               </a>
-              <a href="http://localhost/SIAKAD/Daftar/guru">
+              <a href="http://localhost/SIAKAD/Absen/guru">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
                 <h5 style="color: black; text-align: left;" class=<?php echo $guru?>>Absensi Kelas</h5>
                </div>
               </a>
-              <a href="http://localhost/SIAKAD/Profil/siswa">
+              <a href="http://localhost/SIAKAD/Daftar/siswa">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
-                <h5 style="color: black; text-align: left;" class=<?php echo $profil?>>Daftar Siswa</h5>
+                <h5 style="color: black; text-align: left;" class=<?php echo $guru?>>Daftar Siswa</h5>
                </div>
               </a>
-              <a href="http://localhost/SIAKAD/Profil/siswa">
+              <a href="http://localhost/SIAKAD/Daftar/guru">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
-                <h5 style="color: black; text-align: left;" class=<?php echo $profil?>>Daftar Guru</h5>
+                <h5 style="color: black; text-align: left;" class=<?php echo $guru?>>Daftar Guru</h5>
                </div>
               </a>
-              <a href="http://localhost/SIAKAD/Profil/siswa">
+              <a href="http://localhost/SIAKAD/Profil/guru">
                  <div style="width: 200px; height: 33px; background: #ebebe0; margin-left: 50px; margin-top: 10px; margin-bottom: 5px; padding: 5px 5px 5px 20px;" class="row">
                 <span class="kotak"></span>
                 <span class="triangle"></span>
