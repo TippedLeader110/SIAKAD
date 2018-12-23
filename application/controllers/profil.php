80273a924e27ec36f92a69e66d050ded9d087b04
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-23 16:23:10 +0700

    update profil guru

--- application/controllers/profil.php
+++ application/controllers/profil.php
@@ -17,6 +17,20 @@ class Profil extends CI_Controller {
 		$data['profil'] = 'aktif';
 		$this->load->view('layout/home', $data);
 	}
+	public function siswa1()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/profilsiswa1';
+		$data['nav'] = 'layout/navbar-kiri-siswa';
+		$data['nama'] = 'Profil';
+		$data['beranda'] = '';
+		$data['nilai'] = '';
+		$data['absen'] = '';
+		$data['rapot'] = '';
+		$data['guru'] = '';
+		$data['profil'] = 'aktif';
+		$this->load->view('layout/home', $data);
+	}
 	public function guru()
 	{
 		$data['border']='transparent';
@@ -31,10 +45,24 @@ class Profil extends CI_Controller {
 		$data['profil'] = 'aktif';
 		$this->load->view('layout/home', $data);
 	}
+	public function guru1()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/profilguru1';
+		$data['nav'] = 'layout/navbar-kiri-guru';
+		$data['nama'] = 'Profil';
+		$data['beranda'] = '';
+		$data['nilai'] = '';
+		$data['absen'] = '';
+		$data['rapot'] = '';
+		$data['guru'] = '';
+		$data['profil'] = 'aktif';
+		$this->load->view('layout/home', $data);
+	}
 	public function walikelas()
 	{
 		$data['border']='transparent';
-		$data['page']='userview/profilwalikelas';
+		$data['page']='userview/profilwalikelas1';
 		$data['nav'] = 'layout/navbar-kiri-guru';
 		$data['nama'] = 'Profil';
 		$data['beranda'] = '';
