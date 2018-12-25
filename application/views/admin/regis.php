3b223906b60521e18470c931bcec0cdf568dc68a
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-25 16:37:46 +0700

    regis

--- application/views/admin/regis.php
+++ application/views/admin/regis.php
@@ -8,11 +8,11 @@
 	<div class="row" style="background: white;padding-top: 20px;">
 		<div class="col-md-12">
 			<table class="table table-borderless">
-				<td><h4>Data Sekolah</h4></td>
+				<td><h4><center>Data Sekolah</center></h4></td>
 					<?php  if (validation_errors()!="") {
 					echo "<div class='alert'></span><strong>ERROR !</strong> ".validation_errors()." </div>";
 					} ?>
-				<td>&nbsp;&nbsp;&nbsp;</td><td><h4>Data Orangtua</h4></td>
+				<td><h4><center>Data Orangtua</center></h4></td>
 			</table>
 			
 		</div>
@@ -23,16 +23,32 @@
 				<form method="post" action="<?php echo base_url() ?>admin/simpanm">
 				<table class="table table-borderless">
 					<tr>
-						<td><input type="number" name="nis" placeholder="NIS"></td>
-						<td><b>Data Ayah</b></td>
+						<td><h5>NIS</h5></td>
+						<td><h5>Nama Ayah</h5></td>
 					</tr>
 					<tr>
-						<td><input type="text" name="username" placeholder="Username"></td>
+						<td><input type="number" name="nis" placeholder="NIS"></td>
 						<td><input type="text" placeholder="Nama Ayah" name="ayah"></td>
 					</tr>
 					<tr>
-						<td><input type="text" name="pass" placeholder="Password"></td>
+						<td><h5>Username</h5></td>
+						<td><h5>Pendidikan Ayah</h5></td>
+					</tr>
+					<tr>
+						<td><input type="text" name="username" placeholder="Username"></td>
 						<td><input type="text" placeholder="Pendidikan Ayah" name="penayah"></td>
+					</tr>
+					<tr>
+						<td><h5>Password</h5></td>
+						<td><h5>Pekerjaan Ayah</h5></td>
+					</tr>
+					<tr>
+						<td><input type="text" name="pass" placeholder="Password"></td>
+						<td><input type="text" name="payah" placeholder="Pekerjaan Ayah"></td>
+					</tr>
+					<tr>
+						<td><h5>Jurusan</h5></td>
+						<td><h5>Alamat Ayah</h5></td>
 					</tr>
 					<tr>
 						<td><select name="jurusan" class="form-control" style="width: 400px;">
@@ -40,31 +56,53 @@
 							<option value="IPA">IPA</option>
 							<option value="IPS">IPS</option>
 						</select></td>
-						<td><input type="text" name="payah" placeholder="Pekerjaan Ayah"></td>
-					</tr>
-					<tr>
-						<td><input type="text" name="tahun" placeholder="Tahun Angkatan"></td>
 						<td><input type="text" name="aayah" placeholder="Alamat Ayah"></td>
 					</tr>
 					<tr>
-						<td><input type="text" name="sekolah" placeholder="Asal Sekolah"></td>
+						<td><h5>Tahun Angkatan</h5></td>
+						<td><h5>Nomor HP Ayah</h5></td>
+					</tr>
+					<tr>
+						<td><input type="text" name="tahun" placeholder="Tahun Angkatan"></td>
 						<td><input type="text" name="nohpa" placeholder="Nomor HP Ayah"></td>
 					</tr>
 					<tr>
-						<td><h4>Data Pribadi</h4></td>
-						<td><b>Data Ibu</b></td>
+						<td><h5>Asal Sekolah</h5></td>
+						<td><h5></h5></td>
+					</tr>
+					<tr>
+						<td><input type="text" name="sekolah" placeholder="Asal Sekolah"></td>
+					</tr>
+					<tr>
+						<td><h4><center>Data Pribadi</center></h4></td>
+					</tr>
+					<tr>
+						<td><h5>Nama Murid</h5></td>
+						<td><h5>Nama Ibu</h5></td>
 					</tr>
 					<tr>
 						<td><input type="text" name="nama" placeholder="Nama Murid"></td>
 						<td><input type="text" name="ibu" placeholder="Nama Ibu"></td>
 					</tr>
 					<tr>
+						<td><h5>Tempat Lahir</h5></td>
+						<td><h5>Pekerjaan Ibu</h5></td>
+					</tr>
+					<tr>
 						<td><input type="text" name="tlahir" placeholder="Tempat Lahir Anda"></td>
 						<td><input type="text" name="penibu" placeholder="Pendidikan Ibu"></td>
 					</tr>
 					<tr>
+						<td><h5>Tanggal Lahir</h5></td>
+						<td><h5>Pekerjaan Ibu</h5></td>
+					</tr>
+					<tr>
 						<td><input type="date" name="date" placeholder="Tanggal Lahir"></td>
 						<td><input type="text" name="pibu" placeholder="Pekerjaan Ibu"></td>
+					</tr>
+					<tr>
+						<td><h5>Jenis Kelamin</h5></td>
+						<td><h5>Alamat Ibu</h5></td>
 					</tr>
 					<tr>
 						<td><select name="jk" class="form-control" style="width: 400px;">
@@ -75,17 +113,30 @@
 						<td><input type="text" name="aibu" placeholder="Alamat Ibu"></td>
 					</tr>
 					<tr>
+						<td><h5>Alamat Murid</h5></td>
+						<td><h5>Nomor HP Ibu</h5></td>
+					</tr>
+					<tr>
 						<td><input type="text" name="alamat" placeholder="Alamat Murid"></td>
 						<td><input type="text" name="nohpi" placeholder="Nomor HP Ibu"></td>
 					</tr>
 					<tr>
+						<td><h5>Email</h5></td>
+					</tr>
+					<tr>
 						<td><input type="text" name="email" placeholder="Email"></td>
+					</tr>
+					<tr>
+						<td><h5>Agama</h5></td>
 					</tr>
 					<tr>
 						<td><select name="agama" class="form-control" style="width: 400px;">
        						<option>Agama</option>
       						<option value="Islam">Islam</option>
      					</select></td>
+					</tr>
+					<tr>
+						<td><h5>Anak ke Berapa</h5></td>
 					</tr>
 					<tr>
 						<td><input type="number" name="anak" placeholder="Anak ke berapa"></td>
