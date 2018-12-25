6c822bfe2163651a55a689f2aa660ff1e1314d48
Nurul Andini <39187373+nurulandini@users.noreply.github.com>
2018-12-25 22:59:35 +0700

    update abseninput.php

--- application/views/userview/abseninput.php
+++ application/views/userview/abseninput.php
@@ -1,139 +1,67 @@
 <div class="container" style="border-width:0px; background-color:#DCDDE1; padding-top: 10px; padding-bottom: 10px;">
-<div class="container" style="border-width:1px; background-color:#DCDDE1;">
-	<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;">
-		<h5 align="center"><b>DATA SISWA</b></h5>	
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-					<table style="min-width: 500px;">
-						<tr>
-							<td>Nama Siswa</td>
-							<td><input type="text" class="form-control" placeholder="Nama Siswa"style="width: 250px;" ></td>
-						</tr>
-						<tr>
-							<td>NIS</td>
-							<td><input type="text" class="form-control" placeholder="NIS"style="width: 250px;" ></td>
-						</tr>
-						<tr>
-							<td>Jurusan</td>
-							<td>
-								<select class="form-control" style="width: 250px;">
-								<option value="jurusan">-Pilih Jurusan-</option>
-								<option value="jurusan">Ilmu Pengetahuan Alam</option>
-								<option value="jurusan">Ilmu Pengetahuan Sosial</option>
-								</select> 
-							</td>
-						</tr>
-					</table>
-				</div>
-				<div class="col-md-6">
-					<table style="min-width: 500px;">
-						<tr>
-							<td>Kelas</td>
-							<td>
-								<select class="form-control" style="width: 250px;">
-								<option value="kelas">-Pilih Kelas-</option>
-								<option value="kelas">X IPA 1</option>
-								<option value="kelas">X IPA 2</option>
-								<option value="kelas">X IPA 3</option>
-								<option value="kelas">X IPS 1</option>
-								<option value="kelas">X IPS 2</option>
-								<option value="kelas">X IPS 3</option>
-								<option value="kelas">XI IPA 1</option>
-								<option value="kelas">XI IPA 2</option>
-								<option value="kelas">XI IPA 3</option>
-								<option value="kelas">XI IPS 1</option>
-								<option value="kelas">XI IPS 2</option>
-								<option value="kelas">XI IPS 3</option>
-								<option value="kelas">XII IPA 1</option>
-								<option value="kelas">XII IPA 2</option>
-								<option value="kelas">XII IPA 3</option>
-								<option value="kelas">XII IPS 1</option>
-								<option value="kelas">XII IPS 2</option>
-								<option value="kelas">XII IPS 3</option>
-								</select>
-							</td>
-						</tr>
-						<tr>
-							<td>Semester</td>
-							<td>
-								<select class="form-control" style="width: 250px;">
-									<option value="semester">-Pilih Semester-</option>
-									<option value="semester">Semester Ganjil</option>
-									<option value="semester">Semester Genap</option>
-								</select>
-							</td>
-						</tr>
-						<tr>
-							<td>Tahun Pelajaran</td>
-							<td><input type="text" class="form-control" placeholder="Tahun Pelajaran"style="width: 250px;" ></td>
-						</tr>
-					</table> 
-				</div>
-			</div>
-		</div>	
-		<div style="background-color:#DCDDE1; width:200px; height:10px; margin-left:350px; margin-top:-15px; margin-bottom: 20px;"></div>
-	</div>
-</div>
-</div>
-
-<div style="width:200px; height:30px; margin-left:350px; margin-top:-15px;"></div>
-
-<div class="container" style="border-width:0px; background-color:#DCDDE1; padding-top: 10px; padding-bottom: 10px;">
-<div class="container" style="border-width:1px; background-color:#DCDDE1;">
-	<div style="background-color:#DCDDE1; width:250px; height:30px; margin-left:325px; margin-top:-15px;">
-		<h5 align="center"><b>DATA NILAI ABSENSI</b></h5>	
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-					<table style="min-width: 310px;">
-						<tr>
-							<td>Jumlah Kehadiran</td>
-							<td><input type="text" class="form-control" placeholder="Masukkan Nilai"style="width: 100px;"></td>
-							<td>Hari</td>
-						</tr>
-						<tr>
-							<td>Ketidakhadiran</td>
-						</tr>
-						<table style="margin-left: 110px; min-width: 200px;">
-							<tr>
-								<td>Izin</td>
-								<td><input type="text" class="form-control" placeholder="Masukkan Nilai"style="width: 100px;"></td>
-								<td>Hari</td>
-							</tr>
-							<tr>
-								<td>Sakit</td>
-								<td><input type="text" class="form-control" placeholder="Masukkan Nilai"style="width: 100px;"></td>
-								<td>Hari</td>
-							</tr>
-							<tr>
-								<td>Alpa</td>
-								<td><input type="text" class="form-control" placeholder="Masukkan Nilai"style="width: 100px;"></td>
-								<td>Hari</td>
-							</tr>
-						</table>
-					</table>
-				</div>
-			</div>
-		</div>	
-		<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;"></div>
-	</div>
-</div>
-</div>
-
-<div class="row">
-	<div class="col-md-2">
-		<div class="container" style="border-width:0px; color:white; font-weight:500; margin-left: 365px; margin-top: 30px;">
-			<center><b><a href="#" class="btn btn-primary">Simpan</a></b></center>
+	<div class="container" style="border-width:1px; background-color:#DCDDE1;padding-bottom: 100px">
+		<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;">
+			<h5 align="center"><b>INPUT PRESENSI</b></h5>
 		</div>
-	</div>
-	<div class="col-md-2">
-		<div class="container" style="border-width:0px; font-weight:500; color:white; margin-left: 290px; margin-top: 30px;">
-			<center><b><a href="#" class="btn btn-danger">Batal</a></b></center>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-6" style="padding-top: 10px; margin-right: -200px;margin-left: 20px">
+						<form>
+							<table>
+								<tr><td><h4>Jurusan</h4></td></tr>
+								<tr>
+									<td>
+										<select name="jurusan" class="form-control" style="width: 250px;">
+											<option value="jurusan"><h4>-Pilih Jurusan-</h4></option>
+											<option value="ipa"><h4>Ilmu Pengetahuan Alam</h4></option>
+											<option value="ips"><h4>Ilmu Pengetahuan Sosial</h4></option>
+										</select> 
+									</td>
+								</tr>
+							</table>
+						</form>
+					</div>
+					<div class="col-md-6" style="padding-top: 10px;padding-bottom: 10px; margin-left: 50px;">
+						<table style="min-width: 500px;margin-left: -70px;">
+							<tr><td><h4>Kelas</h4></td></tr>
+							<tr>
+								<td>
+									<select name="kelas" class="form-control" style="width: 250px;">
+										<option value="kelas">-Pilih Tingkat-</option>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+									</select> 
+								</td>
+								<td><button type="submit" class="btn btn-primary" style="min-width: 100px;margin-left: 0px;margin-right: 100px">Cari</button></td>
+							</tr>
+						</table> 
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="container" style="border-width: 0px;">
+				<div class="row">
+					<table  class="table table-bordered table-stripped" style="margin-left: 30px;background: white;margin-right: 30px;text-align: center;">
+						<tr>
+							<th>NIS</th>
+							<th>Nama</th>
+							<th>Jurusan</th>
+							<th>Kelas</th>
+							<th>Aksi</th>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><form method="post" action="<?php echo base_url()?>"><button type="submit" class="btn btn-info">Input Rekap Presensi</button></form></td>
+						</tr>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
