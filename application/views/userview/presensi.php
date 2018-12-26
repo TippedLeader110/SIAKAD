f4ae5381db6a8ccff39af65fb0736a0c1b011516
Nurul Andini <39187373+nurulandini@users.noreply.github.com>
2018-12-26 15:35:14 +0700

    create berita.php

--- application/views/userview/presensi.php
+++ application/views/userview/presensi.php
@@ -1,7 +1,7 @@
 <div class="container" style="border-width:0px; background-color:#DCDDE1; padding-top: 10px; padding-bottom: 20px;">
 <div class="container" style="border-width:1px; background-color:#DCDDE1;">
 	<div style="background-color:#DCDDE1; width:200px; height:30px; margin-left:350px; margin-top:-15px;">
-		<h5 align="center"><b>DATA SISWA</b></h5>	
+		<h5 align="center"><b>Rekapitulasi Presensi Siswa</b></h5>	
 	</div>
 	<div class="row">
 		<div class="col-md-12">
