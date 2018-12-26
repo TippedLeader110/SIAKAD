e620cf2183b1e04e5f7a4bb57f2038d878bb9a70
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-26 14:33:35 +0700

    simpan admin dan controller post

--- application/models/adminmodel.php
+++ application/models/adminmodel.php
@@ -141,4 +141,9 @@ class Adminmodel extends CI_Model {
 		$this->db->where(['jurusan' => $ww]);
 		return $query = $this->db->get($w,$number,$offset)->result();		
 	}
+	public function kirimadmin($user,$nama,$pass,$email)
+	{
+		$data = array('username' => $user, 'nama' => $nama,'password' => $pass, 'email' => $email);
+		$this->db->insert('admin', $data);
+	}
 }
