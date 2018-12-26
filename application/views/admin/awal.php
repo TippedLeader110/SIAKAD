0ffaf33d7f77fe86e119e9fe6da2e4cd17c909f4
usokun <alihidayat278@gmail.com>
2018-12-26 18:57:55 +0900

    penambahan tampilan di adm00n

--- application/views/admin/awal.php
+++ application/views/admin/awal.php
@@ -1,12 +1,13 @@
 <div class="container-fluid" onclick="test3()" onmousewheel="test3()" onmouseover="test3()">
 <div class="col-md-10 offset-md-1" >
 <div class="row" style="margin-bottom: 50px;">
-		<div class="container" style="background-color: white; border-width: 1px; border-color: #b8b894; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey;">
+		<div class="container" style="background-color: white; border-width: 1px; border-color: #b8b894; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px white;">
 		<h3 style="color: #2F669F;"><b>Kontrol Murid</b></h3>
 	</div>
 </div>
-<div class="container" style="border-width: 1px; border-color: #b8b894; background-color: white; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey; margin-top: 20px;">
-	<div class="container-fluid"><?php if ($_SESSION['do']!=''): ?>
+<div class="container" style="border-width: 1px; border-color: #b8b894; background-color: white; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px white; margin-top: -40px; margin-bottom: 30px;">
+	<div class="container-fluid">
+		<?php if ($_SESSION['do']!=''): ?>
 								<script type="text/javascript">
 									Swal(
   'Sukses!',
@@ -16,101 +17,159 @@
 								</script>
 								<?php $_SESSION['do']=''; ?>
 							<?php endif ?>
+	<div class="row"  style="color: #2F669F; padding-left: 15px; padding-bottom: 10px;">
+		<h4>Selamat datang, <b><?php echo $_SESSION['admin']; ?></b></h4>
+	</div>
 		<div class="row">
 			<div class="col-md-6">
 				<div class="container" style="background: #f5b041;height: 185px;">
 					<table style="margin-top: 15px;">
 						<tr>
-							<td rowspan="3"><img src="<?php echo base_url() ?>/img/icon/sekolah.png" style="max-height: 144px;max-width: 144px"></td><td><h4>Jumlah Alumni : <?php echo $alum ?></h4></td>
+							<td rowspan="3" colspan="2"><img src="<?php echo base_url() ?>/img/icon/sekolah.png" style="max-height: 144px;max-width: 144px">
+							</td>
+							<td style="padding-left: 10px;">
+								<h4>Jumlah Alumni</h4>
+							</td>
+							<td style="padding-left: 10px;">
+								<h4>:</h4>
+							</td>
+							<td style="padding-left: 10px;">
+								<h4><?php echo $alum ?></h4>
+							</td>
 						</tr>
 						<tr>
-							<td><h4>Murid Aktif : <?php echo $jumlahmurid ?></h4></td>
+							<td style="padding-left: 10px;">
+								<h4>Murid Aktif</h4>
+							</td>
+							<td style="padding-left: 10px;">
+								<h4>:</h4>
+							</td>
+							<td style="padding-left: 10px;">
+								<h4><?php echo $jumlahmurid ?></h4>
+							</td>
 						</tr>
-						<tr><td><h4>Guru Aktif : <?php echo $jumlahguru ?></h4></td></tr>
+						<tr>
+							<td style="padding-left: 10px;">
+								<h4>Guru Aktif</h4>
+							</td>
+							<td style="padding-left: 10px;">
+								<h4>:</h4>
+							</td>
+							<td style="padding-left: 10px;">
+								<h4><?php echo $jumlahguru ?></h4>
+							</td>
+						</tr>
 					</table>
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="container" style="background: #92c9ed;height: 185px;">
-					<table style="margin-top: 15px;">
+					<table style="margin-top: 20px;">
 						<tr>
-							<td rowspan="3"><img src="<?php echo base_url() ?>/img/icon/settings.png" style="max-height: 144px;max-width: 144px"></td><td><h4>Selamat datang <?php echo $_SESSION['admin']; ?></h4></td>
+							<td rowspan="3"><img src="<?php echo base_url() ?>/img/icon/jam.png" style="max-height: 135px;max-width: 135px">
+							</td>
 						</tr>
 						<tr>
-							<td><h4>Waktu Sekarang : <p id="time"></p> </h4></td>
+							<td  style="padding-left: 10px;"><h4>Waktu Sekarang </h4></td>							
 						</tr>
-						<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript">
-var timestamp = '<?=time();?>';
-function updateTime(){
-  $('#time').html(Date(timestamp));
-  timestamp++;
-}
-$(function(){
-  setInterval(updateTime, 1000);
-});
-</script>
+						<tr>
+							<td style="padding-left: 10px;"><h5 id="time"></h5> </td>
+						</tr>
+						<tr>
+							<td>
+								<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+						<script type="text/javascript">
+						var timestamp = '<?=time();?>';
+						function updateTime(){
+						  $('#time').html(Date(timestamp));
+						  timestamp++;
+						}
+						$(function(){
+						  setInterval(updateTime, 1000);
+						});
+						</script>
+							</td>
+						</tr>
+						
 					</table>
 				</div>
 			</div>
 		</div>
 		<div class="row" style="margin-top: 20px;">
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px;">
 						<tr align="center">
-							<td><a href="<?php echo base_url() ?>admin/Daftar_murid"><img src="<?php echo base_url() ?>/img/icon/table.png" style="max-height: 104px;max-width: 104px"></a></td>
+							<td>
+								<div class="circle" style="background-color: white; min-width: 140px; min-height: 140px; padding-top: 10px;" >
+									<a href="<?php echo base_url() ?>admin/Daftar_murid"><img src="<?php echo base_url() ?>/img/icon/table.png" style="max-height: 104px;max-width: 104px"></a>
+								</div>								
+							</td>
 						</tr>
 						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/Daftar_murid">Lihat Semua Murid</a></h4></td>
+							<td style="padding-top: 20px;"><h5><a  style="color: white;" href="<?php echo base_url() ?>admin/Daftar_murid">Lihat Semua Murid</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px">
 						<tr align="center" style="">
-							<td><a href="<?php echo base_url() ?>admin/post"><img src="<?php echo base_url() ?>/img/icon/post.png" style="max-height: 104px;max-width: 104px"></a></td>
+							<td>
+								<div class="circle" style="background-color: white; min-width: 140px; min-height: 140px; padding-top: 10px;" >
+									<a href="<?php echo base_url() ?>admin/post"><img src="<?php echo base_url() ?>/img/icon/post.png" style="max-height: 104px;max-width: 104px"></a>
+								</div>						
+							</td>
 						</tr>
 						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/post">Buat Pengumuman</a></h4></td>
+							<td  style="padding-top: 20px;"><h5><a style="color: white;" href="<?php echo base_url() ?>admin/post">Buat Pengumuman</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px">
 						<tr align="center" style="">
-							<td><a href="<?php echo base_url() ?>admin/TheAdmin"><img src="<?php echo base_url() ?>/img/icon/admin.png" style="max-height: 104px;max-width: 104px"></a></td>
+							<td>
+								<div class="circle" style="background-color: white; min-width: 140px; min-height: 140px; padding-top: 10px;" >
+									<a href="<?php echo base_url() ?>admin/TheAdmin"><img src="<?php echo base_url() ?>/img/icon/admin.png" style="max-height: 104px;max-width: 104px"></a>
+								</div>						
+							</td>
 						</tr>
 						<tr align="center">
-							<td><a href="<?php echo base_url() ?>admin/TheAdmin"><h4>Daftar Admin</a></h4></td>
+							<td  style="padding-top: 20px;"><h5><a style="color: white;" href="<?php echo base_url() ?>admin/post">Daftar Admin</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
+			<!-- BATAS -->
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px">
 						<tr align="center" style="">
-							<td><a href="<?php echo base_url() ?>admin/logout"><img src="<?php echo base_url() ?>/img/icon/logout.png" style="max-height: 104px;max-width: 104px"></a></td>
+							<td>
+								<div class="circle" style="background-color: white; min-width: 140px; min-height: 140px; padding-top: 20px;" >
+									<a href="<?php echo base_url() ?>admin/logout"><img src="<?php echo base_url() ?>/img/icon/logout.png" style="max-height: 104px;max-width: 104px"></a>
+								</div>						
+							</td>
 						</tr>
 						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/logout">logout</a></h4></td>
+							<td  style="padding-top: 20px;"><h5><a style="color: white;" href="<?php echo base_url() ?>admin/post">Keluar</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
+			
 		</div>
 		<div>
 			
@@ -119,7 +178,7 @@ $(function(){
 </div>
 </div>
 </div>
-				<!-- <div class="col-md-3 offset-md-1" style="background: #4cddc0;height: 120px;margin-right: 10px;border-radius: 8px;" >
+				<!-- <div class="col-md-3 offset-md-1" style="background: #4cddc0;height: 140px;margin-right: 10px;border-radius: 8px;" >
 				<div class="row" style="padding-top: 15px;">
 					<div class="col-md-5" style="margin-right: px;">
 						<img src="<?php echo base_url() ?>/img/icon/student-icon.png" style="max-height: 88px;max-width: 88px">
@@ -129,7 +188,7 @@ $(function(){
 						</div>
 					</div>
 			</div>
-				<div class="col-md-3" style="background: #4cddc0;height: 120px;margin-right: 10px;border-radius: 8px;" >
+				<div class="col-md-3" style="background: #4cddc0;height: 140px;margin-right: 10px;border-radius: 8px;" >
 				<div class="row" style="padding-top: 15px;">
 					<div class="col-md-5" style="margin-right: px;">
 						<img src="<?php echo base_url() ?>/img/icon/student-icon.png" style="max-height: 88px;max-width: 88px">
@@ -139,7 +198,7 @@ $(function(){
 						</div>
 					</div>
 			</div>
-				<div class="col-md-3" style="background: #4cddc0;height: 120px;margin-right: 10px;border-radius: 8px;" >
+				<div class="col-md-3" style="background: #4cddc0;height: 140px;margin-right: 10px;border-radius: 8px;" >
 				<div class="row" style="padding-top: 15px;">
 					<div class="col-md-5" style="margin-right: px;">
 						<img src="<?php echo base_url() ?>/img/icon/student-icon.png" style="max-height: 88px;max-width: 88px">
--- application/views/admin/guru.php
+++ application/views/admin/guru.php
@@ -1,26 +1,82 @@
 <div class="container-fluid" onclick="test3()" onmousewheel="test3()" onmouseover="test3()">
 <div class="col-md-10 offset-md-1" >
 <div class="row" style="margin-bottom: 50px;">
-		<div class="container" style="background-color: white; border-width: 1px; border-color: #b8b894; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey;">
-		<h3 style="color: #2F669F;"><b>Kontrol Murid</b></h3>
+		<div class="container" style="background-color: white; border-width: 1px; border-color: #b8b894; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey;  padding-bottom: 20px;">
+		<h3 style="color: #2F669F;"><b>Kontrol Guru</b></h3>
 	</div>
 </div>
-<div class="container" style="border-width: 1px; border-color: #b8b894; background-color: white; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey; margin-top: 20px;">
+<div class="container" style="border-width: 1px; border-color: #b8b894; background-color: white; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey; margin-top: -30px;">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-6">
-				<div class="container" style="background: #98f6b3;height: 185px;">
+			<div class="col-md-12">
+					<div class="row"  style="color: #2F669F; padding-left: 15px; padding-bottom: 10px;">
+						<h4>Selamat datang, <b><?php echo $_SESSION['admin']; ?></b></h4>
+					</div>
+				<div class="container" style="background: #ffa500;height: 185px; margin-bottom: 20px;">
 					<table style="margin-top: 15px;">
 						<tr>
-							<td rowspan="3"><img src="<?php echo base_url() ?>/img/icon/icon-profil-guru.png" style="max-height: 144px;max-width: 144px"></td><td><h4>Total Guru : <?php echo "$jumlahguru"; ?></h4></td>
+							<td rowspan="3" colspan="2" style="padding-left: 10px;"><img src="<?php echo base_url() ?>/img/icon/icon-profil-guru.png" style="max-height: 144px;max-width: 144px"></td>
+							<td style="padding-left: 10px; color: white;">
+								<h4>Total Guru</h4>
+							</td>
+							<td >
+								<h4 style="padding-left: 10px;  color: white;">:</h4>
+							</td>
+							<td>
+								<h4 style="padding-left: 10px;  color: white;"> <?php echo "$jumlahguru"; ?></h4>
+							</td>
 						</tr>
 						<tr>
-							<td><h4>Guru Aktif :</h4></td>
+							<td style="padding-left: 10px;  color: white;">
+								<h4>Guru Aktif</h4>
+							</td>
+							<td style="padding-left: 10px;  color: white;">
+								<h4>:</h4>
+							</td>
+							<td  style="padding-left: 10px;  color: white;">
+								<h4>
+									
+								</h4>
+							</td>
 						</tr>
-						<tr><td><h4>Guru Non Aktif :</h4></td></tr>
+						<tr>
+							<td>
+								<h4  style="padding-left: 10px;  color: white;">Guru Non Aktif	</h4>
+							</td>
+							<td>
+								<h4  style="padding-left: 10px;  color: white;">:</h4>
+							</td>
+							<td>
+								<h4  style="padding-left: 10px;  color: white;">
+									
+								</h4>
+							</td>
+						</tr>						
 					</table>
 				</div>
 			</div>
+			<div class="col-md-3">
+				<div class="container" style="background: #0073e6;height: 220px; padding-bottom: 20px;">
+				<center>
+					<table style="margin-top: 10px">
+						<tr align="center">
+							<td>
+								<div class="circle" style="max-height: 140px; max-width: 140px; min-height: 140px; min-width: 140px; padding-top: 10px;" title="Pengumuman">
+									<a href="<?php echo base_url() ?>admin/post">
+									<img src="<?php echo base_url() ?>/img/icon/post.png" style="max-height: 120px;max-width: 120px">
+								</a>
+								</div>
+								
+							</td>
+						</tr>
+						<tr align="center">
+							<td style="padding-top: 5px; padding-bottom: 5px;"><h5><a href="<?php echo base_url() ?>admin/Daftar_murid" style="text-decoration: none; color:white;">Pengumuman</a></h5></td>
+						</tr>
+					</table>
+				</center>
+				</div>
+			</div>
+			<!--
 			<div class="col-md-3">
 				<div class="container" style="background: white;height: 185px;">
 					<center>
@@ -35,78 +91,73 @@
 				</center>
 				</div>
 			</div>
+			-->
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
-					<center>
-					<table style="margin-top: 20px">
-						<tr align="center" style="">
-							<td><img src="<?php echo base_url() ?>/img/icon/buku.png" style="max-height: 104px;max-width: 104px"></td>
-						</tr>
-						<tr align="center">
-							<td><h4>Daftar Mata Pelajaran</h4></td>
-						</tr>
-					</table>
-				</center>
-				</div>
-			</div>
-		</div>
-		<div class="row" style="margin-top: 20px;">
-			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px">
 						<tr align="center">
-							<td><a href="<?php echo base_url() ?>admin/Daftar_guru"><img src="<?php echo base_url() ?>/img/icon/table.png" style="max-height: 104px;max-width: 104px"></td></a>
+							<td>
+								<div class="circle" style="max-height: 140px; max-width: 140px; min-height: 140px; min-width: 140px; padding-top: 10px;" title="Daftar Guru">
+									<a href="<?php echo base_url() ?>admin/Daftar_guru">
+									<img src="<?php echo base_url() ?>/img/icon/table.png" style="max-height: 120px;max-width: 120px">
+								</a>
+								</div>
+								
+							</td>
 						</tr>
 						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/Daftar_guru">Lihat Semua Guru</a></h4></td>
+							<td style="padding-top: 5px; padding-bottom: 5px;"><h5><a href="<?php echo base_url() ?>admin/Daftar_murid" style="text-decoration: none; color:white;">Lihat Semua Guru</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
+			<!-- -->
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px">
-						<tr align="center" style="">
-							<td><a href="<?php echo base_url() ?>admin/wali"><img src="<?php echo base_url() ?>/img/icon/icon-profil-guru.png" style="max-height: 104px;max-width: 104px"></a></td>
+						<tr align="center">
+							<td>
+								<div class="circle" style="max-height: 140px; max-width: 140px; min-height: 140px; min-width: 140px; padding-top: 20px;" title="Atur Kode Guru">
+									<a href="<?php echo base_url() ?>admin/guruIPA">
+									<img src="<?php echo base_url() ?>/img/icon/class.png" style="max-height: 100px;max-width: 100px">
+								</a>
+								</div>
+								
+							</td>
 						</tr>
 						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/wali">Atur Wali Kelas</a></h4></td>
+							<td style="padding-top: 5px; padding-bottom: 5px;"><h5><a href="<?php echo base_url() ?>admin/guruIPA" style="text-decoration: none; color:white;">Atur Kode Guru</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
+			<!-- -->
 			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
-				<center>
-					<table style="margin-top: 20px">
-						<tr align="center" style="">
-							<td><a href="<?php echo base_url() ?>admin/guruIPA"><img src="<?php echo base_url() ?>/img/icon/class.png" style="max-height: 104px;max-width: 104px"></td></a>
-						</tr>
-						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/guruIPA">Atur Kode Guru</a></h4></td>
-						</tr>
-					</table>
-				</center>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="container" style="background: white;height: 185px;">
+				<div class="container" style="background: #0073e6;height: 220px;">
 				<center>
 					<table style="margin-top: 10px">
-						<tr align="center" style="">
-							<td><a href="<?php echo base_url() ?>admin/gurustatus"><img src="<?php echo base_url() ?>/img/icon/active.png" style="max-height: 104px;max-width: 104px"></td></a>
+						<tr align="center">
+							<td>
+								<div class="circle" style="max-height: 140px; max-width: 140px; min-height: 140px; min-width: 140px; padding-top: 10px;" title="Atur Status Guru">
+									<a href="<?php echo base_url() ?>admin/gurustatus">
+									<img src="<?php echo base_url() ?>/img/icon/active.png" style="max-height: 110px;max-width: 120px">
+								</a>
+								</div>
+								
+							</td>
 						</tr>
 						<tr align="center">
-							<td><h4><a href="<?php echo base_url() ?>admin/gurustatus">Atur Status Guru</a></h4></td>
+							<td style="padding-top: 5px; padding-bottom: 5px;"><h5><a href="<?php echo base_url() ?>admin/gurustatus" style="text-decoration: none; color:white;">Atur Status Guru</a></h5></td>
 						</tr>
 					</table>
 				</center>
 				</div>
 			</div>
+			<!-- -->
 		</div>
 		<div>
 			
