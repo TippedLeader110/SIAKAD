72e0718bf07b2cb8e0282e04b3e6e8592569ed86
usokun <alihidayat278@gmail.com>
2018-12-26 16:04:47 +0900

    fields in title

--- application/views/admin/kode.php
+++ application/views/admin/kode.php
@@ -1,9 +1,11 @@
 <div class="container-fluid" onclick="test3()" onmousewheel="test3()" onmouseover="test3()">
 <div class="col-md-10 offset-md-1" >
 <div class="row" style="margin-bottom: 50px;">
-		<h3>Kontrol Murid</h3>
-		<hr>
+	<div class="container" style="background-color: white; border-width: 1px; border-color: #b8b894; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey;">
+		<h3 style="color: #2F669F;"><b>Kontrol Murid</b></h3>
+	</div>
 </div>
+<div class="container" style="border-width: 1px; border-color: #b8b894; background-color: white; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey; margin-top: 20px;">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-12">
@@ -36,7 +38,7 @@
 	</div>
 </div>
 </div>
-
+</div>
 <?php 
 if (isset($_POST['jurusan'])) {
 	if ($_POST['jurusan']=='ipa') {
--- application/views/admin/test.php
+++ application/views/admin/test.php
@@ -2,8 +2,9 @@
 <div class="container-fluid" onclick="test3()" onmousewheel="test3()" onmouseover="test3()">
 <div class="col-md-10 offset-md-1" >
 <div class="row" style="margin-bottom: 50px;">
-		<h3>Daftar Murid</h3>
-		<hr>
+	<div class="container" style="background-color: white; border-width: 1px; border-color: #b8b894; padding: 15px 15px 10px 10px; box-shadow: 0.5px 0.5px 0.5px 0.5px grey;">
+			<h3 style="color: #2F669F;"><b>Daftar Murid</b></h3>
+		</div>
 </div>
 	<div class="row" style="background: white;padding-top: 20px;">
 		<div class="col-md-12">
