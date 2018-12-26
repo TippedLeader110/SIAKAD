cfcf2859307d3dc28bffcce5bf57db54725dae53
TippedLeader110 <bayhaqi101@gmail.com>
2018-12-26 14:51:12 +0700

    Revert "Update nilai.php"
    This reverts commit a2575c5da66063897600545b22b2c49a43bfc8bd.

--- application/controllers/nilai.php
+++ application/controllers/nilai.php
@@ -242,7 +242,15 @@ class Nilai extends CI_Controller {
 		$data['border']='black';
 		$data['page']='userview/uas';
 		$data['nama'] = 'Daftar Nilai';
+		if ($_SESSION['wali']=='') {
+		$data['nav'] = 'layout/navbar-kiri-guru';
+		}
+		elseif ($_SESSION['nis']!='') {
 		$data['nav'] = 'layout/navbar-kiri-siswa';
+		}
+		else{
+			$data['nav'] = 'layout/navbar-kiri-walikelas';	
+		}
 		$data['beranda'] = '';
 		$data['nilai'] = 'aktif';
 		$data['absen'] = '';
