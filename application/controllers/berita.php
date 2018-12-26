ec8f19634e57c792bb4a2200ad3094e6a0cd54e8
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-26 15:51:53 +0700

    nih nurul ge be el ka

---
+++ application/controllers/berita.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Berita extends CI_Controller {
+
+	public function post(){
+		$data['border']='black';
+		$data['nama'] = 'Beranda';
+		if ($_SESSION['wali']=='') {
+		$data['nav'] = 'layout/navbar-kiri-guru';
+		}
+		elseif ($_SESSION['nis']=='') {
+			$data['nav'] = 'layout/navbar-kiri-murid';
+		}
+		else{
+			$data['nav'] = 'layout/navbar-kiri-walikelas';	
+		}
+		$data['page']='userview/nilai';
+		$data['beranda'] = 'aktif';
+		$data['nilai'] = '';
+		$data['absen'] = '';
+		$data['rapot'] = '';
+		$data['guru'] = '';
+		$data['profil'] = '';
+		$this->load->view('layout/home', $data);
+	}
+}
