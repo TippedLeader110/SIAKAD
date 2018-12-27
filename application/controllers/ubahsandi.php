99675317f647fe0762741c9ec6577e5921351da8
TippedLeader110 <bayhaqi101@gmail.com>
2018-12-27 20:25:09 +0700

    Update ubahsandi.php

--- application/controllers/ubahsandi.php
+++ application/controllers/ubahsandi.php
@@ -7,20 +7,45 @@ class Ubahsandi extends CI_Controller {
 	{
 		$data['border']='transparent';
 		$data['page']='userview/ubahsandi';
-		$this->load->view('layout/main', $data);
+
+		$this->load->view('layout/home', $data);
 	}
 
 	public function guru()
 	{
 		$data['border']='transparent';
+		if ($_SESSION['nis']!='') {
+			$data['nav'] = 'layout/navbar-kiri-siswa';
+		}
+		elseif($_SESSION['nip']!='')
+		{
+			if ($_SESSION['wali']!='') {
+				$data['nav'] = 'layout/navbar-kiri-walikelas';
+			}
+			else{
+				$data['nav'] = 'layout/navbar-kiri-guru';
+			}
+		}
 		$data['page']='userview/gantipasswordguru';
-		$this->load->view('layout/main', $data);
+		$this->load->view('layout/home', $data);
 	}
 
 	public function siswa()
 	{
 		$data['border']='transparent';
+		if ($_SESSION['nis']!='') {
+			$data['nav'] = 'layout/navbar-kiri-siswa';
+		}
+		elseif($_SESSION['nip']!='')
+		{
+			if ($_SESSION['wali']!='') {
+				$data['nav'] = 'layout/navbar-kiri-walikelas';
+			}
+			else{
+				$data['nav'] = 'layout/navbar-kiri-guru';
+			}
+		}
 		$data['page']='userview/ubahsandi';
-		$this->load->view('layout/main', $data);
+		$this->load->view('layout/home', $data);
 	}
 }
