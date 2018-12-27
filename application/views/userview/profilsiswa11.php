3556fd4c185ff441a2f052a1a5c327f703e5afa5
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-27 20:48:57 +0700

    profil gambar

--- application/views/userview/profilsiswa11.php
+++ application/views/userview/profilsiswa11.php
@@ -6,17 +6,17 @@
 	<?php foreach ($arra as $key => $v) {} ?>
 		<div class="row">
 				<div class="col-md-4">
-					<div class="container" style="background-color:#8DB3DA; border-width: 0px; color:white; font-weight: 500;width: 150px; margin-top: 30px;">
+					<div class="container" style="background-color:#8DB3DA; border-width: 0px; color:white; font-weight: 500; margin-top: 30px; font-size: 20px; width: 170px;">
 						<center><a href="<?php echo base_url() ?>Profil/tampilmurid" style="text-decoration: none;">Data Sekolah</a></center>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="container" style="background-color:#2F669F; border-width: 0px; font-weight: 500; margin-top: 30px; margin-bottom: 15px;">
+					<div class="container" style="background-color:#2F669F; border-width: 0px; font-weight: 500; margin-top: 30px; margin-bottom: 15px; font-size: 20px; width: 170px;">
 						<center><a href="<?php echo base_url() ?>Profil/tampilmurid1" style="color:white; text-decoration: none;">Data Pribadi</a></center>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="container" style="background-color:#8DB3DA; border-width: 0px; font-weight: 500;margin-left: 10px;  margin-top: 30px; margin-bottom: 15px;">
+					<div class="container" style="background-color:#8DB3DA; border-width: 0px; font-weight: 500;margin-left: 10px;  margin-top: 30px; margin-bottom: 15px; font-size: 20px; width: 170px;">
 						<center><a href="<?php echo base_url() ?>Profil/tampilmurid2" style="text-decoration: none;">Data Orangtua</a></center>
 					</div>
 				</div>
@@ -100,7 +100,8 @@
 	<div class="col-md-4">
 		<div class="container" style="background-color:#DCDDE1; border-width: 0px; font-weight: 500; max-width: 200px; padding-top: 5px; padding-bottom: 5px; margin-top: 77px;">
 			<div class="circle-2" style="margin: 10px; margin-top: 10px;">
-				<img src="https://static.thenounproject.com/png/630729-200.png" style="max-width: 150px; max-height: 150px;">
+				<img src="<?php echo base_url() ?>gambar/<?php echo $v->pict ?>.png" style="max-width: 150px; max-height: 150px;">
+			
 			</div>
 		</div>
 	</div>
