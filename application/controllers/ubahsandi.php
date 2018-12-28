acb6fcf0dc9dd3ec8b11fd5cb1e736cf025111bb
yoraisme <39548274+yoraisme@users.noreply.github.com>
2018-12-28 09:10:46 +0700

    ubahsandi

--- application/controllers/ubahsandi.php
+++ application/controllers/ubahsandi.php
@@ -13,7 +13,7 @@ class Ubahsandi extends CI_Controller {
 
 	public function guru()
 	{
-		$data['nama'] = 'Ubah Sandi Guru';
+		$data['nama'] = 'Ubah Sandi Siswa';
 		$data['border']='transparent';
 		if ($_SESSION['nis']!='') {
 			$data['nav'] = 'layout/navbar-kiri-siswa';
